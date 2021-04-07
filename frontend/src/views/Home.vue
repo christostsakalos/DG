bad25361516125c537322885bcf90ded0a2211be
christsakalos94@gmail.com <christsakalos94@gmail.com>
2021-04-07 01:22:32 +0100

    Authentication

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+   <h1 class="title">Home Page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
